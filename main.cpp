bccf0c8bf2a29dbfda49386b02d7fc71d1468376
beforelight <1761690868@qq.com>
2020-11-07 21:56:53 +0800

    pre for AHRS

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "remote_i2c.h"
 #include "drv_imu_invensense.hpp"
-
+#include <sys/time.h>
 remote_i2c iic("/dev/i2c-1");
 
 inv::i2cInterface_t my_i2c(
@@ -17,6 +17,12 @@ float gyro[3] = {0, 0, 0};
 float mag[3] = {0, 0, 0};
 float temp = 0;
 
+
+int usGet(void){
+    struct timeval tic;
+    gettimeofday(&tic,NULL);
+    return tic.tv_usec+tic.tv_sec*1000*1000;
+}
 
 int example1(int argc, const char **argv) {
     inv::imuPtr_t my_imu;
@@ -74,12 +80,66 @@ int example2(int argc, const char **argv) {
     return 0;
 }
 
+int example3(int argc, const char **argv) {
+    inv::imuPtr_t my_imu;
+    if (0 == my_imu.Load(my_i2c)) {
+        if (my_imu->Init() == 0) {
+            //自检时保持静止，否则会直接失败
+            if (my_imu->SelfTest() == 0) {
+                int us_p,us_n;
+                us_n=usGet();
+                int times=100;
+                while(times--) {
+                    while(!static_cast<inv::mpuSeries_t *>(my_imu.get())->DataReady()){
+//                        my_imu->ReadSensorBlocking();
+                    }
+                }
+                us_p=usGet();
+                printf("avg %dus\r\n",(us_p-us_n)/100);
+
+                us_n=usGet();
+                times=100;
+                while(times--) {
+                    while(!static_cast<inv::mpuSeries_t *>(my_imu.get())->DataReady()){
+                        my_imu->ReadSensorBlocking();
+                    }
+                }
+                us_p=usGet();
+                printf("avg %dus\r\n",(us_p-us_n)/100);
+
+            } else {
+                printf("自检未通过\r\n");
+            }
+        } else {
+            printf("初始化未通过\r\n");
+        }
+    } else {
+        printf("没有imu\r\n");
+    }
+    return 0;
+}
+
+int example4(int argc, const char **argv) {
+    inv::imuPtr_t my_imu;
+    int ustime=usGet();
+    if (0 == my_imu.Load(my_i2c)&&(0==my_imu->Init())&&0==my_imu->SelfTest()){
+        while((usGet()-ustime)>10*1000){
+            my_imu->ReadSensorBlocking();
+            my_imu->Convert(acc, acc + 1, acc + 2, gyro, gyro + 1, gyro + 2);
+            printf("accel \t%.3f \t%.3f \t%.3f m/s^2\t", acc[0], acc[1], acc[2]);
+            printf("gyro \t%.3f \t%.3f \t%.3f dps \r\n", gyro[0], gyro[1], gyro[2]);
+        }
+    }
+    return 0;
+}
 
 int main(int argc, const char **argv) {
-    printf("\r\n*****************example1*****************\r\n");
-    example1(argc, argv);
-    printf("\t\n*****************example2*****************\r\n");
-    example2(argc, argv);
+//    printf("\r\n*****************example1*****************\r\n");
+//    example1(argc, argv);
+//    printf("\t\n*****************example2*****************\r\n");
+//    example2(argc, argv);
+//    printf("\t\n*****************example3*****************\r\n");
+    example4(argc, argv);
     printf("Hello\r\n");
 
     return 0;
