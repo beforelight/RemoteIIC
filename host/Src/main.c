378172c8b0a0d1cfc375e783a9ce40436a2809e1
beforelight <1761690868@qq.com>
2021-04-21 18:25:02 +0800

    host 更新引脚

--- host/Src/main.c
+++ host/Src/main.c
@@ -281,8 +281,18 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pins : GPIO0_Pin GPIO1_Pin GPIO2_Pin */
-  GPIO_InitStruct.Pin = GPIO0_Pin|GPIO1_Pin|GPIO2_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIO0_GPIO_Port, GPIO0_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : GPIO0_Pin */
+  GPIO_InitStruct.Pin = GPIO0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIO0_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : GPIO1_Pin GPIO2_Pin */
+  GPIO_InitStruct.Pin = GPIO1_Pin|GPIO2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
